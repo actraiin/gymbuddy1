fecb664f742fb7bc39c6f928a43455eb47f219f1
vagrant <vagrant@precise32.(none)>
2015-03-04 03:49:46 +0000

    added time window

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -33,13 +33,13 @@ exports.viewMatches = function (req, res) {
             {
                 if ((myProfiles[i]["spottingRange"] == profile[j]["spottingRange"] ||
                     myProfiles[i]["runningRange"] == profile[j]["runningRange"]) &&
-                    ((myProfiles[i]["monday"] == profile[j]["monday"] && profile[j]["monday"] != "") ||
-                     (myProfiles[i]["tuesday"] == profile[j]["tuesday"] && profile[j]["tuesday"] != "") ||
-                     (myProfiles[i]["wednesday"] == profile[j]["wednesday"] && profile[j]["wednesday"] != "") ||
-                     (myProfiles[i]["thursday"] == profile[j]["thursday"] && profile[j]["thursday"] != "")||
-                    (myProfiles[i]["friday"] == profile[j]["friday"] && profile[j]["friday"] != "") ||
-                    (myProfiles[i]["saturday"] == profile[j]["saturday"] && profile[j]["saturday"] != "")||
-                    (myProfiles[i]["sunday"] == profile[j]["sunday"] && profile[j]["sunday"] != "") ))
+                    (checkDayMatch(myProfiles[i]["monday"], profile[j]["monday"]) ||
+                     checkDayMatch(myProfiles[i]["tuesday"], profile[j]["tuesday"]) ||
+                     checkDayMatch(myProfiles[i]["wednesday"], profile[j]["wednesday"]) ||
+                     checkDayMatch(myProfiles[i]["thursday"], profile[j]["thursday"])||
+                    checkDayMatch(myProfiles[i]["friday"], profile[j]["friday"]) ||
+                    checkDayMatch(myProfiles[i]["saturday"], profile[j]["saturday"])||
+                    checkDayMatch(myProfiles[i]["sunday"], profile[j]["sunday"])))
                     
                 {
                     if (!existsInArray(matches,profile[j]))
@@ -57,6 +57,29 @@ exports.viewMatches = function (req, res) {
         {
             if (arr[i] == item)
                 return true;
+        }
+        return false;
+    }
+    
+    function checkDayMatch(day1, day2)
+    {
+        if (day1 == "" || day2 == "")
+        {
+            return false;
+        }
+        
+        var day1H = day1.substring(0,day1.indexOf(":"));
+        var day1M = day1.substring(day1.indexOf(":")+1);
+        
+        var day2H = day2.substring(0,day2.indexOf(":"));
+        var day2M = day2.substring(day2.indexOf(":")+1);
+        
+        var day1TotalMinutes = parseInt(day1H) * 60 + parseInt(day1M);
+        var day2TotalMinutes = parseInt(day2H) * 60 + parseInt(day2M);
+        
+        if ((day1TotalMinutes + 15 >= day2TotalMinutes) && (day1TotalMinutes - 15 <= day2TotalMinutes))
+        {
+            return true;
         }
         return false;
     }
